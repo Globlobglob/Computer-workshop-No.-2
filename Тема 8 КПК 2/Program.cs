79da61a5af818e2698484aaf9057255cbeeefa3a
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-11-20 02:40:15 +0200

    Add files via upload

---
+++ Тема 8 КПК 2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        /*
+        static int Func(int x)
+        {
+            for(int i=0; i <= x; i++)
+            {
+                Write("#");
+            }
+
+            return x;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Write("Enter a x= ");
+            int x=int.Parse(Console.ReadLine());
+
+            int result=Func(x);
+            Console.WriteLine(result);  
+        }*/
+
+        /*static int Func(int value_1)
+        {
+            for (int i = 0; i <value_1; i++)
+            {
+                for (int h = 0; h < i; h++)
+                {
+                    Write("#");
+                }
+                Console.WriteLine();
+            }
+            return value_1;
+        }
+
+
+
+
+
+      static void Main(string[] args)
+        {
+            Write("Enter triangle size= ");
+            int size=int.Parse(Console.ReadLine());
+
+            int res=Func(size); 
+        }
+
+        */
+
+
+      /* static double  Func(double x)
+        {
+           
+            x = (Math.Sqrt(5)+5)/2 + (Math.Sqrt(12)+12)/2 + (Math.Sqrt(19)+19)/2;
+            return x;   
+
+        }
+
+        static void Main(string[] args)
+        {
+            double res = Func(0);
+            Console.WriteLine(res);
+        }
+      */
+
+       /* static int Func(int a , int b)
+        {
+            return Math.Max(a, b);
+        }
+         static void Main(string[] args) 
+        {
+            Write("Enter value_1= ");
+                int val1 = int.Parse(Console.ReadLine()); 
+            
+
+            Write("Enter value_2= ");
+                int val2=int.Parse(Console.ReadLine());
+
+
+
+            int res = Func(val1, val2);
+            Console.WriteLine(res);
+        }
+       */
+
+
+
+
+
+    }
+}
